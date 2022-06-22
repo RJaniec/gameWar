ea7a46d4238516fd93403f175329f594d7e4f807
RJaniec <rafael.janiec@outlook.com>
2022-06-22 13:27:55 +0200

    Initial Commit

---
+++ gameWar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gameWar
+//
+//  Created by Rafał Janiec on 22/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gameWar/gameWarApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameWarApp.swift
+//  gameWar
+//
+//  Created by Rafał Janiec on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct gameWarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
